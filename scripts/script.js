f673fbb1e40342552ca6598d53c6855e91883212
see-you-space-cowboy <sirdan89@gmail.com>
2022-12-27 22:00:06 -0500

    12-27-22

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,7 @@
+
+/*
+slideshow
+*/
 let images = ['images/slideshow1.jpg','images/slideshow2.jpg','images/slideshow3.jpg'];
 let i =0;
 function slideShow() {
@@ -14,3 +18,33 @@ setTimeout("slideShow()", 3000);
 }
 
 window.onload = slideShow;
+
+/*
+navbar
+*/
+
+const menu = document.querySelector(".list");
+const menuItems = document.querySelectorAll(".bar");
+const hamburger= document.querySelector(".hamburger");
+const closeIcon= document.querySelector(".closeIcon");
+const menuIcon = document.querySelector(".menuIcon");
+
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
+}
+
+hamburger.addEventListener("click", toggleMenu);
+
+menuItems.forEach( 
+  function(menuItem) { 
+    menuItem.addEventListener("click", toggleMenu);
+  }
+)
